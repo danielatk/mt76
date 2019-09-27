31ea57329e5beb6bb9837f2f5068424ecf37bf0d
Daniel Atkinson Oliveira <danatkhoo@yahoo.com.br>
2019-09-27 18:26:53 -0300

    mudando buffer rx de 128 pra 256 (default) e tx de 2048 (default) pra 10

--- mt76.h
+++ mt76.h
@@ -16,9 +16,9 @@
 #include <net/mac80211.h>
 #include "util.h"
 
-#define MT_TX_RING_SIZE     128
+#define MT_TX_RING_SIZE     256
 #define MT_MCU_RING_SIZE    32
-#define MT_RX_BUF_SIZE      2048
+#define MT_RX_BUF_SIZE      10
 #define MT_SKB_HEAD_LEN     128
 
 struct mt76_dev;
